c160f71b6e3fe3370bf5d7a520db1fac246f7c6d
Ahad <ahoudtiion@hotmail.com>
2023-10-05 12:31:57 +0300

    Initial Commit

---
+++ water tracker app/water_tracker_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  water_tracker_appApp.swift
+//  water tracker app
+//
+//  Created by Ahad on 20/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct water_tracker_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
